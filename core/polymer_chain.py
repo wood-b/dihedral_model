d6c69f01f3df5475aed2b1ab1da5ed8cc0252fc0
wood-b <b.wood@berkeley.edu>
2016-10-03 12:57:10 -0700

    charged subclass added

--- core/polymer_chain.py
+++ core/polymer_chain.py
@@ -1,22 +1,21 @@
 import numpy as np
 import math
 from utils import utils
-from utils.stats import ArrayStats
+from utils.stats import Stats, ArrayStats
 
 __author__ = "Brandon Wood"
 
 
 class Polymer(object):
     def __init__(self, monomer_num, monomer_length, link_length,
-                 link_angle, n_prob_angle, sample_num=None, c_prob_angle=None):
+                 link_angle, prob_angle, sample_num=None):
         """
         :param monomer_num: number of monomers in polymer chain
         :param monomer_length: length of monomer tangent line
         :param link_length: length of link between monomers
         :param link_angle: angle between monomer tangent and link
-        :param n_prob_angle: array of dihedral probability and angle pairs (neutral case)
+        :param prob_angle: array of dihedral probability and angle pairs
         :param sample_num: number of chains to sample
-        :param c_prob_angle: array of dihedral probability and angle pairs (charged case)
         """
         self.chain = np.zeros(((2 * monomer_num), 3))
         self.relax_chain = np.zeros(((2 * monomer_num), 3))
@@ -24,13 +23,10 @@ class Polymer(object):
         self.monomer_len = monomer_length
         self.link_len = link_length
         self.link_angle = (link_angle * np.pi / 180)
-        self.n_prob_angle = n_prob_angle
+        self.prob_angle = prob_angle
         self.sample_num = sample_num
-        self.c_prob_angle = c_prob_angle
         self.end_to_end = None
-        self.c_ete = None
         self.corr = None
-        self.c_corr = None
         self.ete_stats = ArrayStats(self.monomer_num + 1)
         self.corr_stats = ArrayStats(self.monomer_num)
         # position monomer tangent and links
@@ -58,19 +54,20 @@ class Polymer(object):
 
     def _n_random_angle(self):
         random = np.random.uniform(0.0, 1.0, size=(self.monomer_num - 1))
-        angle_map = self.n_prob_angle[:, 0].searchsorted(random)
+        angle_map = self.prob_angle[:, 0].searchsorted(random)
         dihedral_set = []
         for prob_i in angle_map:
             rand = np.random.uniform(0.0, 1.0)
             if rand < 0.5:
-                dihedral_set.append(self.n_prob_angle[prob_i - 1][1])
+                dihedral_set.append(self.prob_angle[prob_i - 1][1])
                 continue
             else:
                 try:
-                    dihedral_set.append(self.n_prob_angle[prob_i][1])
+                    dihedral_set.append(self.prob_angle[prob_i][1])
                 except IndexError:
+                    # needs to be general
                     if prob_i == 3600:
-                        dihedral_set.append(self.n_prob_angle[0][1])
+                        dihedral_set.append(self.prob_angle[0][1])
         return dihedral_set
 
     def rotate_chain(self):
@@ -100,6 +97,20 @@ class Polymer(object):
             self.ete_stats.update(float(chain_i), self.end_to_end)
             self.corr_stats.update(float(chain_i), self.corr)
 
+
+class RandomChargePolymer(Polymer):
+    def __init__(self, monomer_num, monomer_length, link_length,
+                 link_angle, prob_angle, c_prob_angle, sample_num=None):
+        super(RandomChargePolymer, self).__init__(monomer_num, monomer_length,
+                                                  link_length, link_angle,
+                                                  prob_angle, sample_num)
+        self.c_prob_angle = c_prob_angle
+        self.charged_chain = None
+        self.c_end_to_end = None
+        self.c_corr = None
+        self.c_ete_stats = Stats()
+        self.c_corr_stats = Stats()
+
     def _c_random_angle(self, sites):
         random = np.random.uniform(0.0, 1.0, size=sites)
         angle_map = self.c_prob_angle[:, 0].searchsorted(random)
@@ -116,7 +127,7 @@ class Polymer(object):
                         dihedral_set.append(self.c_prob_angle[0][1])
         return dihedral_set
 
-    def pick_links(self, sites):
+    def _pick_links(self, sites):
         link_pos = []
         excluded = []
         while len(link_pos) < sites:
@@ -128,37 +139,31 @@ class Polymer(object):
                 excluded.extend([test_link - 1, test_link, test_link + 1])
         return sorted(link_pos)
 
-    def relax_charged_chain(self, electrons, values=False):
+    def relax_charged_chain(self, electrons):
         self.rotate_chain()
         # electron affects dihedral angle of 3 links
         sites = int(math.ceil(electrons / 2.0)) * 3
         dihedral_set = self._c_random_angle(sites)
-        charged_chain = np.array(self.relax_chain, copy=True)
-        pick_links = zip(self.pick_links(sites), dihedral_set)
+        self.charged_chain = np.array(self.relax_chain, copy=True)
+        pick_links = zip(self._pick_links(sites), dihedral_set)
         for link, angle in pick_links:
             # idx is the bead or atom index on the chain
             idx = (link * 2) - 1
-            uv = utils.unit_vector(charged_chain[idx], charged_chain[idx + 1])
-            for pos_j in range(idx + 2, len(charged_chain), 1):
-                charged_chain[pos_j] = utils.point_rotation(charged_chain[pos_j], angle, uv, origin=charged_chain[idx])
+            uv = utils.unit_vector(self.charged_chain[idx], self.charged_chain[idx + 1])
+            for pos_j in range(idx + 2, len(self.charged_chain), 1):
+                self.charged_chain[pos_j] = utils.point_rotation(self.charged_chain[pos_j],
+                                                                 angle, uv, origin=self.charged_chain[idx])
         # sampling
-        self.c_ete = (math.sqrt(np.dot(
-            charged_chain[len(charged_chain)-1] - charged_chain[0],
-            charged_chain[len(charged_chain)-1] - charged_chain[0])))
+        self.c_end_to_end = (math.sqrt(np.dot(
+            self.charged_chain[len(self.charged_chain)-1] - self.charged_chain[0],
+            self.charged_chain[len(self.charged_chain)-1] - self.charged_chain[0])))
         self.c_corr = np.array([utils.correlation(
-            charged_chain[0], charged_chain[1],
-            charged_chain[i], charged_chain[i+1])
+            self.charged_chain[0], self.charged_chain[1],
+            self.charged_chain[i], self.charged_chain[i+1])
                                  for i in range(0, len(self.chain), 2)])
-        if values:
-            return self.relax_chain, self.end_to_end, charged_chain, self.c_ete
 
     def sample_charged_chains(self, electrons):
-        ave_ete = 0.0
-        ave_corr = np.zeros(self.monomer_num)
-        for chain_i in range(self.sample_num):
+        for chain_i in range(1, self.sample_num + 1, 1):
             self.relax_charged_chain(electrons)
-            ave_ete += self.c_ete
-            ave_corr += self.c_corr
-        ave_ete /= self.sample_num
-        ave_corr /= self.sample_num
-        return ave_ete, ave_corr
+            self.c_ete_stats.update(float(chain_i), self.c_end_to_end)
+            self.c_corr_stats.update(float(chain_i), self.c_corr)
