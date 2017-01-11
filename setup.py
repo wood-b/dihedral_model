a830c66ed1c2d41e2483f892af69d8cfab4f395c
wood-b <b.wood@berkeley.edu>
2017-01-11 11:56:37 -0800

    required packages updated

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="dihedral_model",
+    version="1.0.1",
+    author="Brandon Wood",
+    author_email="b.wood@berkeley.edu",
+    install_requires=["pymatgen>=4.5.4"],
+    extras_require={"babel": ["openbabel", "pybel"]},
     packages=find_packages(),
-    version="1.0"
+
 )
