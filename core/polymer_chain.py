f145af272803efe4a8e3337d4acf92c8b7af0704
wood-b <b.wood@berkeley.edu>
2017-03-09 13:25:05 -0800

    polaron model removed, random order added

--- core/polymer_chain.py
+++ core/polymer_chain.py
@@ -166,7 +166,7 @@ class RandomChargePolymer(Polymer):
         self.c_corr_stats = Stats()
         # set of dihedral angles for charged polymer
         self.c_dihedral_set = []
-        self.actual_percent_excited = None
+        self.shuffle_dihedral_set = []
         self.c_links = None
 
     def _c_random_angle(self, total_sites):
@@ -184,54 +184,26 @@ class RandomChargePolymer(Polymer):
                     if prob_i == len(self.c_prob_angle):
                         self.c_dihedral_set.append(self.c_prob_angle[0][1])
 
-    def _pick_links(self, sites, polaron_size):
-        # links are indexed from 1
-        total_sites = self.monomer_num - 1
-        neutral_sites = total_sites - (sites * polaron_size)
-        num_neutral_sites = []
-        for i in range(1000):
-            neutral_spacing = np.random.uniform(size=(sites + 1)) * sites * 10
-            num_neutral_sites = np.rint(neutral_spacing * (neutral_sites / sum(neutral_spacing)))
-            if sum(num_neutral_sites) == neutral_sites:
-                break
-        if sum(num_neutral_sites) != neutral_sites:
-            print "picking links error"
-        links = []
-        # places the charged links
-        for i in range(len(num_neutral_sites) - 1):
-            if i == 0:
-                links.extend(np.arange(1, (polaron_size + 1)) + num_neutral_sites[i])
-            else:
-                links.extend(np.arange(1, (polaron_size + 1)) + (links[-1] + num_neutral_sites[i]))
-        self.c_links = links
-
-    def rotate_charged_chain(self, percent_excited, polaron_size):
-        self.rotate_chain()
-        # percent_excited is the desired percentage of dihedral angles impacted by excitation
-        # polaron_size is number of sequential dihedral angles affected by an excitation
-        sites = int(math.floor(((percent_excited / 100.) * (self.monomer_num - 1.)) / float(polaron_size)))
-        if sites == 0:
-            print "percent excited is too low"
-        if sites != 0:
-            self.actual_percent_excited = ((sites * polaron_size) / (self.monomer_num - 1.) * 100.)
-            self._c_random_angle(sites * polaron_size)
-            self.charged_chain = np.array(self.relax_chain, copy=True)
-            self._pick_links(sites, polaron_size)
-            pick_links = zip(self.c_links, self.c_dihedral_set)
-            for link, angle in pick_links:
-                # idx is the bead or atom index on the chain
-                idx = int((link * 2.) - 1.)
-                uv = utils.unit_vector(self.charged_chain[idx], self.charged_chain[idx + 1])
-                for pos_j in range(idx + 2, len(self.charged_chain), 1):
-                    self.charged_chain[pos_j] = utils.point_rotation(self.charged_chain[pos_j],
-                                                                     angle, uv, origin=self.charged_chain[idx])
-        # sampling
+    def shuffle_charged_chain(self, tot_charged_sites):
+        self._c_random_angle(tot_charged_sites)
+        self._random_angle()
+        neutral_sites = (self.monomer_num - 1) - tot_charged_sites
+        self.shuffle_dihedral_set = self.dihedral_set[:neutral_sites] + self.c_dihedral_set
+        np.random.shuffle(self.shuffle_dihedral_set)
+        self.charged_chain = np.array(self.chain, copy=True)
+        pos_i = 1
+        for angle in self.shuffle_dihedral_set:
+            uv = utils.unit_vector(self.charged_chain[pos_i], self.charged_chain[pos_i + 1])
+            for pos_j in range(pos_i + 2, len(self.charged_chain), 1):
+                self.charged_chain[pos_j] = utils.point_rotation(self.charged_chain[pos_j],
+                                                                 angle, uv, origin=self.charged_chain[pos_i])
+            pos_i += 2
         self.c_end_to_end = (math.sqrt(np.dot(
             self.charged_chain[-1] - self.charged_chain[0],
             self.charged_chain[-1] - self.charged_chain[0])))
 
-    def sample_charged_chains(self, percent_excited, polaron_size):
+    def sample_charged_chains(self, tot_charged_sites):
         for chain_i in range(1, self.sample_num + 1, 1):
-            self.rotate_charged_chain(percent_excited, polaron_size)
+            self.shuffle_charged_chain(tot_charged_sites)
             self.p2_order_param(self.charged_chain)
             self.c_ete_stats.update(self.c_end_to_end)
--- scripts/run_partial_order_param.py
+++ scripts/run_partial_order_param.py
@@ -21,61 +21,53 @@ def run_partial_order_param():
     parser.add_argument('-ll', action='store', type=float, default=1.480, help='link_length')
     parser.add_argument('-la', action='store', type=float, default=15.0, help='link_angle')
     parser.add_argument('-sn', action='store', type=int, required=True, help='sample_number')
-    parser.add_argument('-pe', action='store', type=int, required=True, help='percent excited')
-    parser.add_argument('-es', action='store', type=int, required=True, help='excitation size')
+    parser.add_argument('-cs', action='store', type=int, required=True, help='number of charged dihedrals')
     parser.add_argument('-o', action='store', type=str, required=True, help='output filename')
     parser.add_argument('-od', action='store', type=str, required=True, help='output directory')
 
     args = parser.parse_args()
 
     # import
-    energy, dihedral, errors = import_qchem.get_energy_dihedral(args.nf)
     c_energy, c_dihedral, c_errors = import_qchem.get_energy_dihedral(args.cf)
+    energy, dihedral, errors = import_qchem.get_energy_dihedral(args.nf)
     # fit dihedral potential curve
-    rel_eV_energy = utils.relative_energy(energy)
     c_rel_eV_energy = utils.relative_energy(c_energy)
-    params, covar = curve_fit(utils.RB_potential, dihedral, rel_eV_energy)
+    rel_eV_energy = utils.relative_energy(energy)
     c_params, c_covar = curve_fit(utils.RB_potential, c_dihedral, c_rel_eV_energy)
+    params, covar = curve_fit(utils.RB_potential, dihedral, rel_eV_energy)
     # create list of angles and corresponding energies
     angles = np.linspace(-179.9, 180.0, args.gp)
-    RB_energy = [utils.RB_potential(angle, *params) for angle in angles]
     c_RB_energy = [utils.RB_potential(angle, *c_params) for angle in angles]
+    RB_energy = [utils.RB_potential(angle, *params) for angle in angles]
     # Boltzmann distribution
-    prob = utils.boltz_dist(args.t, RB_energy)
     c_prob = utils.boltz_dist(args.t, c_RB_energy)
+    prob = utils.boltz_dist(args.t, RB_energy)
     # cumulative probability
-    cum_prob = [sum(prob[0:prob_i]) for prob_i in range(len(prob))]
-    prob_angle = np.array(zip(cum_prob, angles))
     c_cum_prob = [sum(c_prob[0:c_prob_i]) for c_prob_i in range(len(c_prob))]
     c_prob_angle = np.array(zip(c_cum_prob, angles))
+
+    cum_prob = [sum(prob[0:prob_i]) for prob_i in range(len(prob))]
+    prob_angle = np.array(zip(cum_prob, angles))
     # run dihedral model
-    if args.pe == 0:
-        poly = Polymer(args.mn, args.ml, args.ll, args.la, prob_angle, args.sn)
-        poly.sample_chains()
-    if args.pe == 100:
-        poly = Polymer(args.mn, args.ml, args.ll, args.la, c_prob_angle, args.sn)
-        poly.sample_chains()
-    if args.pe != 0 and args.pe != 100:
-        poly = RandomChargePolymer(args.mn, args.ml, args.ll, args.la, prob_angle, c_prob_angle, args.sn)
-        poly.sample_charged_chains(args.pe, args.es)
+    poly = RandomChargePolymer(args.mn, args.ml, args.ll, args.la, prob_angle, c_prob_angle, args.sn)
+    poly.sample_charged_chains(args.cs)
 
     # write files
     for attr, value in poly.__dict__.iteritems():
         if attr.startswith('c_ete_stats'):
-            utils.write_json(value.mean, "{dir}/{name}_m{m}_t{t}_{s}_pe{p}_{d}_mean.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, p=args.pe, d=attr))
-            utils.write_json(value.std_error, "{dir}/{name}_m{m}_t{t}_{s}_pe{p}_{d}_std_error.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, p=args.pe, d=attr))
+            utils.write_json(value.mean, "{dir}/{name}_m{m}_t{t}_{s}_cs{cs}_{d}_mean.json".format(
+                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, cs=args.cs, d=attr))
+            utils.write_json(value.variance, "{dir}/{name}_m{m}_t{t}_{s}_cs{cs}_{d}_var.json".format(
+                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, cs=args.cs, d=attr))
+            utils.write_json(value.std_error, "{dir}/{name}_m{m}_t{t}_{s}_cs{cs}_{d}_std_error.json".format(
+                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, cs=args.cs, d=attr))
         if attr.startswith('s_order_param'):
-            utils.write_json(value.mean, "{dir}/{name}_m{m}_t{t}_{s}_pe{p}_{d}_mean.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, p=args.pe, d=attr))
-            utils.write_json(value.variance, "{dir}/{name}_m{m}_t{t}_{s}_pe{p}_{d}_var.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, p=args.pe, d=attr))
-            utils.write_json(value.std_error, "{dir}/{name}_m{m}_t{t}_{s}_pe{p}_{d}_std_error.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, p=args.pe, d=attr))
-        if attr.startswith('actual_percent_excited'):
-            utils.write_json(value, "{dir}/{name}_m{m}_t{t}_{s}_pe{p}_{d}.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, p=args.pe, d=attr))
+            utils.write_json(value.mean, "{dir}/{name}_m{m}_t{t}_{s}_cs{cs}_{d}_mean.json".format(
+                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, cs=args.cs, d=attr))
+            utils.write_json(value.variance, "{dir}/{name}_m{m}_t{t}_{s}_cs{cs}_{d}_var.json".format(
+                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, cs=args.cs, d=attr))
+            utils.write_json(value.std_error, "{dir}/{name}_m{m}_t{t}_{s}_cs{cs}_{d}_std_error.json".format(
+                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, cs=args.cs, d=attr))
 
 if __name__ == '__main__':
     run_partial_order_param()
