87af7539d36c52b94ca2402a16a02bbdef65259f
wood-b <b.wood@berkeley.edu>
2017-06-05 11:05:50 -0700

    dihedral import class added

--- utils/import_qchem.py
+++ utils/import_qchem.py
@@ -1,25 +1,40 @@
-import numpy as np
-from pymatgen import Molecule
-from pymatgen.io.qchem import QcTask, QcOutput
+from pymatgen.io.qchem import QcOutput
 import os
-import re
+from scipy.optimize import curve_fit
+import utils
 
-def get_energy_dihedral(directory):
-    energy, dihedral, errors = [], [], []
-    for f in os.listdir(directory):
-        if ".qcout" in f:
-            if ".orig" not in f:
-                try:
-                    output = QcOutput('{d}/{f}'.format(d=directory,f=f))
-                    qchem_in = output.data[-1]['input']
+
+class ImportDihedralPotential(object):
+
+    def __init__(self, directory, dihedral_angles):
+        """
+        directory: where all the qchem out files are stored
+        angles: list of angles to evaluate energies using RB function
+
+        """
+        self.dir = directory
+        self.angles = dihedral_angles
+        self.abs_energy, self.dihedral, self.error = self._get_energy_dihedral()
+        self.energy = utils.relative_energy(self.abs_energy)
+        self.params, self.covar = curve_fit(utils.RB_potential, self.dihedral, self.energy)
+        self.RB_energy = [utils.RB_potential(angle, *self.params) for angle in self.angles]
+
+    def _get_energy_dihedral(self):
+        energy, dihedral, errors = [], [], []
+        for f in os.listdir(self.dir):
+            if ".qcout" in f:
+                if ".orig" not in f:
                     try:
-                        energy.append(output.final_energy)
-                        constraints = qchem_in.params['opt']
-                        for l in constraints:
-                            if 'tors' in l:
-                                dihedral.append(l[len(l) - 1])
-                    except IndexError:
-                        errors.append('no energy in {f}'.format(f=f))
-                except AttributeError:
-                    errors.append('unknown error in {f}'.format(f=f))
-    return energy, dihedral, errors
+                        output = QcOutput('{d}/{f}'.format(d=self.dir, f=f))
+                        qchem_in = output.data[-1]['input']
+                        try:
+                            energy.append(output.final_energy)
+                            constraints = qchem_in.params['opt']
+                            for l in constraints:
+                                if 'tors' in l:
+                                    dihedral.append(l[len(l) - 1])
+                        except IndexError:
+                            errors.append('no energy in {f}'.format(f=f))
+                    except AttributeError:
+                        errors.append('unknown error in {f}'.format(f=f))
+        return energy, dihedral, errors
