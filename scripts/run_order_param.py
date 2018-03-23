660b5495a51bd4d71b44dd4a25a329d9342fbe6a
Brandon Wood <b.wood@berkeley.edu>
2018-03-22 19:26:36 -0700

    update run scripts, take prob_angle JSON, and write single JSON dictionary

--- scripts/run_order_param.py
+++ scripts/run_order_param.py
@@ -1,8 +1,7 @@
 import numpy as np
-from scipy.optimize import curve_fit
-from utils import import_qchem
 from utils import utils
 from core.polymer_chain import Polymer
+from core.polymer_chain import RandomChargePolymer
 from argparse import ArgumentParser
 
 
@@ -10,51 +9,57 @@ def run_order_param():
 
     description = "command line interface for running dihedral_model"
     parser = ArgumentParser(description=description)
-    parser.add_argument('-f', action='store', type=str, help='input file', required=True)
-    parser.add_argument('-gp', action='store', type=int, default=3600,
-                        help='number of grid points used for dihedral angles')
-    parser.add_argument('-t', action='store', type=float, required=True, help='temperature in kelvin')
+    parser.add_argument('-gf', action='store', type=str, default=None, help='ground state prob angle JSON file')
+    parser.add_argument('-cf', action='store', type=str, default=None, help='charged or excited prob angle JSON file')
     parser.add_argument('-mn', action='store', type=int, required=True, help='monomer_number')
     parser.add_argument('-ml', action='store', type=float, default=2.548, help='monomer_length')
     parser.add_argument('-ll', action='store', type=float, default=1.480, help='link_length')
     parser.add_argument('-la', action='store', type=float, default=15.0, help='link_angle')
     parser.add_argument('-sn', action='store', type=int, required=True, help='sample_number')
+    parser.add_argument('-nc', action='store', type=int, required=True, help='number of charged or excited dihedrals')
     parser.add_argument('-o', action='store', type=str, required=True, help='output filename')
     parser.add_argument('-od', action='store', type=str, required=True, help='output directory')
 
     args = parser.parse_args()
 
-    # import
-    energy, dihedral, errors = import_qchem.get_energy_dihedral(args.f)
-    # fit dihedral potential curve
-    rel_eV_energy = utils.relative_energy(energy)
-    params, covar = curve_fit(utils.RB_potential, dihedral, rel_eV_energy)
-    # create list of angles and corresponding energies
-    angles = np.linspace(-179.9, 180.0, args.gp)
-    RB_energy = [utils.RB_potential(angle, *params) for angle in angles]
-    # Boltzmann distribution
-    prob = utils.boltz_dist(args.t, RB_energy)
-    # cumulative probability
-    cum_prob = [sum(prob[0:prob_i]) for prob_i in range(len(prob))]
-    prob_angle = np.array(zip(cum_prob, angles))
+    # import ground state - Probability angle
+    if args.gf:
+        prob_angle = np.array(utils.read_json(args.gf))
+    # import excited - Probability angle
+    if args.cf:
+        c_prob_angle = np.array(utils.read_json(args.cf))
+
     # run dihedral model
-    poly = Polymer(args.mn, args.ml, args.ll, args.la, prob_angle, args.sn)
-    poly.sample_chains()
+    if args.nc == 0:
+        poly = Polymer(args.mn, args.ml, args.ll, args.la, prob_angle, args.sn)
+        for chain_i in range(1, args.sn + 1, 1):
+            poly.rotate_chain()
+            poly.p2_order_param(poly.relax_chain)
+
+    if args.nc == (args.mn - 1):
+        poly = Polymer(args.mn, args.ml, args.ll, args.la, c_prob_angle, args.sn)
+        for chain_i in range(1, args.sn + 1, 1):
+            poly.rotate_chain()
+            poly.p2_order_param(poly.relax_chain)
+
+    if args.nc != 0 and args.nc != (args.mn - 1):
+        poly = RandomChargePolymer(args.mn, args.ml, args.ll, args.la, prob_angle, c_prob_angle, args.sn)
+        for chain_i in range(1, args.sn + 1, 1):
+            poly.shuffle_charged_chain(args.nc)
+            poly.p2_order_param(poly.charged_chain)
 
     # write files
+    run_dict = {}
     for attr, value in poly.__dict__.iteritems():
-        if attr.startswith('ete_stats'):
-            utils.write_json(value.mean.tolist(), "{dir}/{name}_m{m}_t{t}_{s}_{d}_mean.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, d=attr))
-            utils.write_json(value.variance.tolist(), "{dir}/{name}_m{m}_t{t}_{s}_{d}_var.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, d=attr))
         if attr.startswith('s_order_param'):
-            utils.write_json(value.mean, "{dir}/{name}_m{m}_t{t}_{s}_{d}_mean.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, d=attr))
-            utils.write_json(value.variance, "{dir}/{name}_m{m}_t{t}_{s}_{d}_var.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, d=attr))
-            utils.write_json(value.std_error, "{dir}/{name}_m{m}_t{t}_{s}_{d}_std_error.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, d=attr))
+            s_order_param_dict = {'mean': value.mean, 'variance': value.variance, 'std_error': value.std_error}
+            run_dict['s_order_param'] = s_order_param_dict
+
+    utils.write_json(run_dict, "{dir}/{name}_mn{m}_nc{nc}_sn{s}_s_order_param.json".format(dir=args.od,
+                                                                                           name=args.o,
+                                                                                           m=args.mn,
+                                                                                           nc=args.nc,
+                                                                                           s=args.sn))
 
 
 if __name__ == '__main__':
--- scripts/run_tangent_corr.py
+++ scripts/run_tangent_corr.py
@@ -1,6 +1,4 @@
 import numpy as np
-from scipy.optimize import curve_fit
-from utils import import_qchem
 from utils import utils
 from core.polymer_chain import Polymer
 from core.polymer_chain import RandomChargePolymer
@@ -11,90 +9,80 @@ def run_tangent_corr():
 
     description = "command line interface for running dihedral_model"
     parser = ArgumentParser(description=description)
-    parser.add_argument('-nf', action='store', type=str, help='input file', required=True)
-    parser.add_argument('-cf', action='store', type=str, help='input file', required=True)
-    parser.add_argument('-gp', action='store', type=int, default=3600,
-                        help='number of grid points used for dihedral angles')
-    parser.add_argument('-t', action='store', type=float, required=True, help='temperature in kelvin')
+    parser.add_argument('-gf', action='store', type=str, default=None, help='ground state prob angle JSON file')
+    parser.add_argument('-cf', action='store', type=str, default=None, help='charged or excited prob angle JSON file')
     parser.add_argument('-mn', action='store', type=int, required=True, help='monomer_number')
     parser.add_argument('-ml', action='store', type=float, default=2.548, help='monomer_length')
     parser.add_argument('-ll', action='store', type=float, default=1.480, help='link_length')
     parser.add_argument('-la', action='store', type=float, default=15.0, help='link_angle')
     parser.add_argument('-sn', action='store', type=int, required=True, help='sample_number')
-    parser.add_argument('-cs', action='store', type=int, required=True, help='number of charged dihedrals')
+    parser.add_argument('-nc', action='store', type=int, required=True, help='number of charged or excited dihedrals')
     parser.add_argument('-o', action='store', type=str, required=True, help='output filename')
     parser.add_argument('-od', action='store', type=str, required=True, help='output directory')
 
     args = parser.parse_args()
 
-    # import
-    c_energy, c_dihedral, c_errors = import_qchem.get_energy_dihedral(args.cf)
-    energy, dihedral, errors = import_qchem.get_energy_dihedral(args.nf)
-    # fit dihedral potential curve
-    c_rel_eV_energy = utils.relative_energy(c_energy)
-    rel_eV_energy = utils.relative_energy(energy)
-    c_params, c_covar = curve_fit(utils.RB_potential, c_dihedral, c_rel_eV_energy)
-    params, covar = curve_fit(utils.RB_potential, dihedral, rel_eV_energy)
-    # create list of angles and corresponding energies
-    angles = np.linspace(-179.9, 180.0, args.gp)
-    c_RB_energy = [utils.RB_potential(angle, *c_params) for angle in angles]
-    RB_energy = [utils.RB_potential(angle, *params) for angle in angles]
-    # Boltzmann distribution
-    c_prob = utils.boltz_dist(args.t, c_RB_energy)
-    prob = utils.boltz_dist(args.t, RB_energy)
-    # cumulative probability
-    c_cum_prob = [sum(c_prob[0:c_prob_i]) for c_prob_i in range(len(c_prob))]
-    c_prob_angle = np.array(zip(c_cum_prob, angles))
-    cum_prob = [sum(prob[0:prob_i]) for prob_i in range(len(prob))]
-    prob_angle = np.array(zip(cum_prob, angles))
+    # import ground state - Probability angle
+    if args.gf:
+        prob_angle = np.array(utils.read_json(args.gf))
+    # import excited - Probability angle
+    if args.cf:
+        c_prob_angle = np.array(utils.read_json(args.cf))
+
     # run dihedral model
-    if args.cs == 0:
+    if args.nc == 0:
         poly = Polymer(args.mn, args.ml, args.ll, args.la, prob_angle, args.sn)
         for chain_i in range(1, args.sn + 1, 1):
             poly.rotate_chain()
             poly.tangent_auto_corr(poly.relax_chain)
             poly.ete_stats.update(poly.end_to_end)
+            poly.dihedral_hist.update(poly.dihedral_set)
+            poly.ete_hist.append(poly.end_to_end[-1])
 
-    if args.cs == (args.mn - 1):
+    if args.nc == (args.mn - 1):
         poly = Polymer(args.mn, args.ml, args.ll, args.la, c_prob_angle, args.sn)
         for chain_i in range(1, args.sn + 1, 1):
             poly.rotate_chain()
             poly.tangent_auto_corr(poly.relax_chain)
             poly.ete_stats.update(poly.end_to_end)
+            poly.dihedral_hist.update(poly.dihedral_set)
+            poly.ete_hist.append(poly.end_to_end[-1])
 
-    if args.cs != 0 and args.cs != (args.mn - 1):
+    if args.nc != 0 and args.nc != (args.mn - 1):
         poly = RandomChargePolymer(args.mn, args.ml, args.ll, args.la, prob_angle, c_prob_angle, args.sn)
         for chain_i in range(1, args.sn + 1, 1):
-            poly.shuffle_charged_chain(args.cs)
+            poly.shuffle_charged_chain(args.nc)
             poly.tangent_auto_corr(poly.charged_chain)
             poly.c_ete_stats.update(poly.c_end_to_end)
+            poly.dihedral_hist.update(poly.shuffle_dihedral_set)
+            poly.ete_hist.append(poly.c_end_to_end)
 
-    # write files
+    # write JSON file with dictionary from run
+    run_dict = {}
     for attr, value in poly.__dict__.iteritems():
-        if attr.startswith('ete_stats'):
-            utils.write_json(value.mean.tolist(), "{dir}/{name}_m{m}_t{t}_{s}_cs{cs}_{d}_mean.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, cs=args.cs, d=attr))
-            utils.write_json(value.variance.tolist(), "{dir}/{name}_m{m}_t{t}_{s}_cs{cs}_{d}_var.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, cs=args.cs, d=attr))
-            utils.write_json(value.std_error.tolist(), "{dir}/{name}_m{m}_t{t}_{s}_cs{cs}_{d}_std_error.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, cs=args.cs, d=attr))
-        if attr.startswith('c_ete_stats'):
-            utils.write_json(value.mean, "{dir}/{name}_m{m}_t{t}_{s}_cs{cs}_{d}_mean.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, cs=args.cs, d=attr))
-            utils.write_json(value.variance, "{dir}/{name}_m{m}_t{t}_{s}_cs{cs}_{d}_var.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, cs=args.cs, d=attr))
-            utils.write_json(value.std_error, "{dir}/{name}_m{m}_t{t}_{s}_cs{cs}_{d}_std_error.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, cs=args.cs, d=attr))
         if attr.startswith('tangent_corr'):
-            mean_list = [i.mean for i in value]
-            utils.write_json(mean_list, "{dir}/{name}_m{m}_t{t}_{s}_cs{cs}_{d}_mean.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, cs=args.cs, d=attr))
-            var_list = [i.variance for i in value]
-            utils.write_json(var_list, "{dir}/{name}_m{m}_t{t}_{s}_cs{cs}_{d}_var.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, cs=args.cs, d=attr))
-            std_error_list = [i.std_error for i in value]
-            utils.write_json(std_error_list, "{dir}/{name}_m{m}_t{t}_{s}_cs{cs}_{d}_std_error.json".format(
-                dir=args.od, name=args.o, m=args.mn, t=args.t, s=args.sn, cs=args.cs, d=attr))
+            tc_dict = {'mean': [i.mean for i in value],
+                       'variance': [i.variance for i in value],
+                       'std_error': [i.std_error for i in value]}
+            run_dict['tangent_corr'] = tc_dict
+        if attr.startswith('ete_stats'):
+            ete_stats_dict = {'mean': value.mean.tolist(),
+                              'variance': value.variance.tolist(),
+                              'std_error': value.std_error.tolist()}
+            run_dict['ete_stats'] = ete_stats_dict
+        if attr.startswith('c_ete_stats'):
+            c_ete_stats_dict = {'mean': value.mean, 'variance': value.variance, 'std_error': value.std_error}
+            run_dict['c_ete_stats'] = c_ete_stats_dict
+        if attr.startswith('dihedral_hist'):
+            tor_hist_dict = {'bin_num': value.bin_num, 'bin_start': value.bin_start, 'bin_stop': value.bin_stop,
+                             'bin_width': value.bin_width, 'bin_edges': value.bin_edges.tolist(),
+                             'counts': value.counts.tolist(), 'bins': value.bins.tolist()}
+            run_dict['dihedral_hist'] = tor_hist_dict
+        if attr.startswith('ete_hist'):
+            run_dict['ete_hist'] = value
+    utils.write_json(run_dict, "{dir}/{name}_mn{m}_nc{nc}_sn{s}.json".format(dir=args.od, name=args.o, m=args.mn,
+                                                                             nc=args.nc, s=args.sn))
+
 
 if __name__ == '__main__':
     run_tangent_corr()
